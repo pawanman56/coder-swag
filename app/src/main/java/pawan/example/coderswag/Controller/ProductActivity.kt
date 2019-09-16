c1a81a25dd91722a0344b47df0d7cbe255fdf85c
pawanman56 <pawanman56@gmail.com>
2019-09-16 14:18:29 +0545

    gridView added for Product Adapter

--- app/src/main/java/pawan/example/coderswag/Controller/ProductActivity.kt
+++ app/src/main/java/pawan/example/coderswag/Controller/ProductActivity.kt
@@ -1,17 +1,38 @@
 package pawan.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_product.*
+import pawan.example.coderswag.Adapters.ProductAdapter
 import pawan.example.coderswag.R
+import pawan.example.coderswag.Services.DataService
 import pawan.example.coderswag.Utilities.EXTRA_CATEGORY
 
 class ProductActivity : AppCompatActivity() {
+    lateinit var adapter : ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductAdapter(this, DataService.getProducts(categoryType))
+        productListView.adapter = adapter
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screeenSize = resources.configuration.screenWidthDp
+        if (screeenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
     }
 }
--- app/src/main/java/pawan/example/coderswag/Services/DataService.kt
+++ app/src/main/java/pawan/example/coderswag/Services/DataService.kt
@@ -8,10 +8,22 @@ object DataService {
         Category("SHIRTS", "shirtimage"),
         Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage")
     )
 
     val hats = listOf(
+        Product("Devslopes and Graphic Beanie", "$15", "hat1"),
+        Product("Devslopes and Balck Hat", "$20", "hat2"),
+        Product("Devslopes and Sport Hat", "$20", "hat3"),
+        Product("Devslopes and Blue Beanie", "$15", "hat4"),
+        Product("Devslopes and Graphic Beanie", "$15", "hat1"),
+        Product("Devslopes and Balck Hat", "$20", "hat2"),
+        Product("Devslopes and Sport Hat", "$20", "hat3"),
+        Product("Devslopes and Blue Beanie", "$15", "hat4"),
         Product("Devslopes and Graphic Beanie", "$15", "hat1"),
         Product("Devslopes and Balck Hat", "$20", "hat2"),
         Product("Devslopes and Sport Hat", "$20", "hat3"),
@@ -22,10 +34,26 @@ object DataService {
         Product("Devslopes Hoodie Gray", "$49", "hoodie1"),
         Product("Devslopes Hoodie Red", "$49", "hoodie2"),
         Product("Devslopes Hoodie Black", "$49", "hoodie3"),
+        Product("Devslopes Hoodie Blue", "$49", "hoodie4"),
+        Product("Devslopes Hoodie Gray", "$49", "hoodie1"),
+        Product("Devslopes Hoodie Red", "$49", "hoodie2"),
+        Product("Devslopes Hoodie Black", "$49", "hoodie3"),
+        Product("Devslopes Hoodie Blue", "$49", "hoodie4"),
+        Product("Devslopes Hoodie Gray", "$49", "hoodie1"),
+        Product("Devslopes Hoodie Red", "$49", "hoodie2"),
+        Product("Devslopes Hoodie Black", "$49", "hoodie3"),
         Product("Devslopes Hoodie Blue", "$49", "hoodie4")
     )
 
     val shirts = listOf(
+        Product("Devslopes Shirt Gray", "$20", "shirt1"),
+        Product("Devslopes Shirt Red", "$20", "shirt2"),
+        Product("Devslopes Shirt Black", "$20", "shirt3"),
+        Product("Devslopes Shirt Blue", "$20", "shirt4"),
+        Product("Devslopes Shirt Gray", "$20", "shirt1"),
+        Product("Devslopes Shirt Red", "$20", "shirt2"),
+        Product("Devslopes Shirt Black", "$20", "shirt3"),
+        Product("Devslopes Shirt Blue", "$20", "shirt4"),
         Product("Devslopes Shirt Gray", "$20", "shirt1"),
         Product("Devslopes Shirt Red", "$20", "shirt2"),
         Product("Devslopes Shirt Black", "$20", "shirt3"),
